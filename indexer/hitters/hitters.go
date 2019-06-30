fd7949f2b2f9736f85dd6fac4be34d2741459ce3
Verrazano <hdmscratched@gmail.com>
2019-06-29 20:18:31 -0500

    Added indexers for hitters and nemesis.

---
+++ indexer/hitters/hitters.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+
+	. "github.com/Harrison-Miller/kagstats/indexer"
+)
+
+type HittersIndexer struct {
+}
+
+func (i *HittersIndexer) Name() string {
+	return "top_hitters"
+}
+
+func (i *HittersIndexer) Keys() []IndexKey {
+	var keys []IndexKey
+	keys = append(keys, IndexKey{
+		Name:   "playerID",
+		Table:  "players",
+		Column: "ID",
+	}, IndexKey{
+		Name: "hitter",
+	})
+	return keys
+}
+
+func (i *HittersIndexer) Counters() []string {
+	return []string{"kills"}
+}
+
+func (i *HittersIndexer) Index(kill Kill) []Index {
+	var indices []Index
+	if kill.KillerID.Valid && kill.KillerID.Int64 != kill.VictimID {
+		indices = append(indices, Index{
+			Keys:     []int{int(kill.KillerID.Int64), int(kill.Hitter)},
+			Counters: map[string]int{"kills": 1},
+		})
+	}
+
+	return indices
+}
+
+func main() {
+	indexer := HittersIndexer{}
+	err := Run(&indexer)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ indexer/nemesis/nemesis.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+
+	. "github.com/Harrison-Miller/kagstats/indexer"
+)
+
+type NemesisIndexer struct {
+}
+
+func (i *NemesisIndexer) Name() string {
+	return "nemesis"
+}
+
+func (i *NemesisIndexer) Keys() []IndexKey {
+	var keys []IndexKey
+	keys = append(keys, IndexKey{
+		Name:   "playerID",
+		Table:  "players",
+		Column: "ID",
+	}, IndexKey{
+		Name:   "nemesisID",
+		Table:  "players",
+		Column: "ID",
+	})
+	return keys
+}
+
+func (i *NemesisIndexer) Counters() []string {
+	return []string{"deaths"}
+}
+
+func (i *NemesisIndexer) Index(kill Kill) []Index {
+	var indices []Index
+
+	if kill.KillerID.Valid && kill.KillerID.Int64 != kill.VictimID {
+		indices = append(indices, Index{
+			Keys:     []int{int(kill.VictimID), int(kill.KillerID.Int64)},
+			Counters: map[string]int{"deaths": 1},
+		})
+	}
+
+	return indices
+}
+
+func main() {
+	indexer := NemesisIndexer{}
+	err := Run(&indexer)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
