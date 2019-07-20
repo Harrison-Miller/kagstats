b6dfe8573d472676b4cf39ac84f11f6e2fb79c14
Austin Humphreys <austin.humphreys@digitalreasoning.com>
2019-07-19 21:34:15 -0500

    - More model modifications

--- common/models/models.go
+++ common/models/models.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Player struct {
-	ID            int64  `json:"ID" db:"ID"`
+	ID            int64  `json:"id" db:"ID"`
 	Username      string `json:"username"`
 	Charactername string `json:"charactername"`
 	Clantag       string `json:"clantag"`
@@ -18,7 +18,7 @@ func GetPlayer(playerID int, db *sqlx.DB) (Player, error) {
 }
 
 type Server struct {
-	ID          int64  `json:"ID" db:"ID"`
+	ID          int64  `json:"id" db:"ID"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Gamemode    string `json:"gamemode"`
@@ -28,14 +28,14 @@ type Server struct {
 type Killer = Player
 
 type Kill struct {
-	ID          int64  `json:"ID" db:"ID"`
+	ID          int64  `json:"id" db:"ID"`
 	KillerID    int64  `json:"-" db:"killerID"`
 	VictimID    int64  `json:"-" db:"victimID"`
 	KillerClass string `json:"killerClass" db:"killerClass"`
 	VictimClass string `json:"victimClass" db:"victimClass"`
 	Hitter      int64  `json:"hitter" db:"hitter"`
 	Time        int64  `json:"time" db:"epoch"`
-	ServerID    int64  `json:"serverID" db:"serverID"`
+	ServerID    int64  `json:"serverId" db:"serverID"`
 	TeamKill    bool   `json:"teamKill" db:"teamKill"`
 
 	Player `json:"victim" db:"victim,prefix=victim."`
@@ -43,11 +43,11 @@ type Kill struct {
 }
 
 type Event struct {
-	ID       int64  `json:"ID" db:"ID"`
+	ID       int64  `json:"id" db:"ID"`
 	PlayerID int64  `json:"-" db:"playerID"`
 	Type     string `json:"type"`
 	Time     int64  `json:"time"`
-	ServerID int64  `json:"serverID" db:"serverID"`
+	ServerID int64  `json:"serverId" db:"serverID"`
 
 	Player `json:"player" db:"player,prefix=player."`
 }
