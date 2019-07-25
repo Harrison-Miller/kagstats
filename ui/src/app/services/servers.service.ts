6e021028028ba502b1c3ad78bdba27ef4f05cf3b
Austin Humphreys <austin.humphreys@digitalreasoning.com>
2019-07-24 23:31:46 -0500

    - Fixed a servers thing I broke

--- ui/src/app/services/servers.service.ts
+++ ui/src/app/services/servers.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Server } from '../models';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +10,6 @@ export class ServersService {
   constructor(private http: HttpClient) {}
 
   getServers(): Observable<Server[]> {
-    return this.http.get<{ servers: Server[] }>('/api/servers').pipe(
-      map(response => {
-        return response.servers;
-      })
-    );
+    return this.http.get<Server[]>('/api/servers');
   }
 }
