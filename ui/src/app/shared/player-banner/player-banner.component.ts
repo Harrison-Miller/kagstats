84f6bd396f1d39160cb664d918290421ab7564ba
Verrazano <hdmscratched@gmail.com>
2019-10-12 19:37:21 -0500

    make tooltips consistent

--- ui/src/app/shared/player-banner/player-banner.component.ts
+++ ui/src/app/shared/player-banner/player-banner.component.ts
@@ -2,25 +2,25 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Player } from '../../models';
 
 export const REGISTERED_TOOLTIP = {
-  "r10yBadge": "Playing for over 10 years",
-  "r9yBadge": "Playing for over 9 years",
-  "r8yBadge": "Playing for over 8 years",
-  "r7yBadge": "Playing for over 7 years",
-  "r6yBadge": "Playing for over 6 years",
-  "r5yBadge": "Playing for over 5 years",
-  "r4yBadge": "Playing for over 4 years",
-  "r3yBadge": "Playing for over 3 years",
-  "r2yBadge": "Playing for over 2 years",
-  "r1yBadge": "Playing for 1 year",
-  "r9mBadge": "Playing for 9 months",
-  "r6mBadge": "Playing for 6 months",
-  "r3mBadge": "Playing for 3 months",
-  "r2mBadge": "Playing for 2 months",
-  "r1mBadge": "Playing for 1 month",
-  "r3wBadge": "Playing for 3 weeks",
-  "r2wBadge": "Playing for 2 weeks",
-  "r1wBadge": "Playing for 1 week",
-  "newBadge": "Welcome this player to the game!"
+  "r10yBadge": "playing for over 10 years",
+  "r9yBadge": "playing for over 9 years",
+  "r8yBadge": "playing for over 8 years",
+  "r7yBadge": "playing for over 7 years",
+  "r6yBadge": "playing for over 6 years",
+  "r5yBadge": "playing for over 5 years",
+  "r4yBadge": "playing for over 4 years",
+  "r3yBadge": "playing for over 3 years",
+  "r2yBadge": "playing for over 2 years",
+  "r1yBadge": "playing for 1 year",
+  "r9mBadge": "playing for 9 months",
+  "r6mBadge": "playing for 6 months",
+  "r3mBadge": "playing for 3 months",
+  "r2mBadge": "playing for 2 months",
+  "r1mBadge": "playing for 1 month",
+  "r3wBadge": "playing for 3 weeks",
+  "r2wBadge": "playing for 2 weeks",
+  "r1wBadge": "playing for 1 week",
+  "newBadge": "welcome this player to the game!"
 };
 
 @Component({
